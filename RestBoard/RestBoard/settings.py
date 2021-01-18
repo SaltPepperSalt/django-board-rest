55d33bbb49d74399d317b9f86e82fa31ffcdb5c1
SaltPepperSalt <dksro324@gmail.com>
2021-01-18 18:57:17 +0900

    feat: user model added`

--- RestBoard/RestBoard/settings.py
+++ RestBoard/RestBoard/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bbs',
-    'rest_framework'
+    'rest_framework',
+    'user'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'user.User'
